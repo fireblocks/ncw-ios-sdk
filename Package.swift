dd9e7ffb4a6ae4406b8d07351fa59917c006ac17
Dudi <dsgabay@fireblocks.com>
2024-03-31 16:43:41 +0300

    Version 2.3.7-beta-4 - enforce download service certificates

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.3.7-beta-3/iOS-FireblocksSDK.zip", checksum: "4f84fdcfa5dca3089d53f4ba9f78e56bacff427eb0dd3f7bbcf3de8888c58a80"),
+        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.3.7-beta-4/iOS-FireblocksSDK.zip", checksum: "4d4567db5fc43947a2d6b932556f2d1905e78809efb28e784c3c13710fd434e9"),
         .binaryTarget(name: "MPCFBFramework", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.3.1/iOS-MPCFBFramework.zip", checksum: "ca5e4b10813ed1647b6bc6a9f2c2dd383db9ab4d86ba3081b1088f5670fc9ece"),
     ]
 )
