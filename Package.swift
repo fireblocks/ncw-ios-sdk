002da06fffcf436912ec1b038f13c3f7334b82d4
Dudi <dsgabay@fireblocks.com>
2024-05-23 13:20:32 +0300

    Version 2.5.0-beta4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.5.0-beta3/iOS-FireblocksSDK.zip", checksum: "0f0e5da0af5f407f2e71bb7cfcd1d96ca3c5b591c3fe8152d8d8898fef6d964c"),
+        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.5.0-beta4/iOS-FireblocksSDK.zip", checksum: "409c3c572074bed84c6c522cc1f68cd02c1527a3ec5fccc7a620fd3c87966794"),
         .binaryTarget(name: "MPCFBFramework", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.4.0/iOS-MPCFBFramework.zip", checksum: "40dbc533e87b457fcd575ebe8c55515041762f19fa9e423c2c3a17a4a9940d1f"),
     ]
 )
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Fireblocks NCW iOS SDK 
 
-Version 2.5.0-beta3
+Version 2.5.0-beta4
 
