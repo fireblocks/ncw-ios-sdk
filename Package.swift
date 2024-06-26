cd6b945d84e3242e5515ad4c6b278df742b996fc
Dudi <dsgabay@fireblocks.com>
2024-06-26 17:26:02 +0300

    Version 2.6.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.6.0-beta5/iOS-FireblocksSDK.zip", checksum: "2ebf03e40c55e1ef5545fc11f67883288df6089e5028c82cbef03aaca1fe8552"),
+        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.6.0/iOS-FireblocksSDK.zip", checksum: "cc9d92bc4854067dbe9ff9ea585a6dcfe87c256f021d4698163730b4a5bd9d04"),
         .binaryTarget(name: "MPCFBFramework", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.4.0/iOS-MPCFBFramework.zip", checksum: "40dbc533e87b457fcd575ebe8c55515041762f19fa9e423c2c3a17a4a9940d1f"),
     ]
 )
