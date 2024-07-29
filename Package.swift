a182a09758b97b6a3e60e59b746990b42fc149c8
Dudi <dsgabay@fireblocks.com>
2024-07-29 16:20:56 +0300

    2.7.0-beta5

--- Package.swift
+++ Package.swift
@@ -9,13 +9,12 @@ let package = Package(
     products: [
         .library(
             name: "FireblocksSDK",
-            targets: ["FireblocksSDK"]),
-//        targets: ["FireblocksSDK", "MPCFBFramework"]),
+        targets: ["FireblocksSDK", "MPCFBFramework"]),
     ],
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.7.0-beta4/iOS-FireblocksSDK.zip", checksum: "eaae337b4fb05575f2f734ddeae489d69cc0fe1c60d8aad15e9e3971b7c410d1"),
-//        .binaryTarget(name: "MPCFBFramework", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.7.0-beta2/iOS-MPCFBFramework.zip", checksum: "0ee959a4b6cbe1792552f8db79c7528eda23aac2192c39f4d4e2e80498e18d55")
+        .binaryTarget(name: "FireblocksSDK", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.7.0-beta5/iOS-FireblocksSDK.zip", checksum: "db974dab6557be98c5f4093d4a83a6cd6088bdf4fae487fa117fa9c3885ebff2"),
+        .binaryTarget(name: "MPCFBFramework", url: "https://github.com/fireblocks/ncw-ios-sdk/releases/download/2.7.0-beta2/iOS-MPCFBFramework.zip", checksum: "0ee959a4b6cbe1792552f8db79c7528eda23aac2192c39f4d4e2e80498e18d55")
     ]
 )
